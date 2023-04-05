3a6ca105c49ff83965c530c8d8dca9e49985c768
F. Rafael Alvarez <110036242+Rafael30586@users.noreply.github.com>
2023-04-05 20:16:51 -0300

    Add files via upload

---
+++ Portafolio3/codigo.js
@@ -0,0 +1,52 @@
+let botonWhatsapp;
+let botonGitHub;
+let botonGmail;
+let botonLinkedin;
+let numerosRomanos;
+let calculadora;
+let APIMarvel;
+let porcentajes;
+
+
+
+botonWhatsapp = document.getElementById("botonWhatsapp");
+botonGitHub = document.getElementById("botonGitHub");
+botonGmail = document.getElementById("botonGmail");
+botonLinkedin = document.getElementById("botonLinkedin");
+numerosRomanos = document.getElementById("numerosRomanos");
+calculadora = document.getElementById("calculadora"); //por ahora en privado
+APIMarvel = document.getElementById("APIMarvel"); //por ahora en privado
+porcentajes = document.getElementById("porcentajes");
+
+function numeroTelefono(){
+	alert("351-6754046");
+}
+
+function correoElectronico(){
+	alert("fraq86@gmail.com");
+}
+
+function irAGitHub(){
+	window.open("https://github.com/Rafael30586");
+}
+
+function irALinkedin(){
+	window.open("https://www.linkedin.com/in/fernando-rafael-alvarez-99a90454/");
+}
+
+function irANumerosRomanos(){
+	window.open("https://github.com/Rafael30586/NumerosRomanosEX");
+}
+
+function irAPorcentajes(){
+	window.open("https://github.com/Rafael30586/PorcentajeCalculosEX");
+}
+
+
+botonWhatsapp.addEventListener("click",numeroTelefono);
+botonGmail.addEventListener("click",correoElectronico);
+botonGitHub.addEventListener("click",irAGitHub);
+botonLinkedin.addEventListener("click",irALinkedin);
+
+numerosRomanos.addEventListener("click",irANumerosRomanos);
+porcentajes.addEventListener("click",irAPorcentajes);

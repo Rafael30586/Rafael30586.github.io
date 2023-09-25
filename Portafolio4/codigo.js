7a80f553e629a72bce32b251d09496ac7d783381
F. Rafael Alvarez <110036242+Rafael30586@users.noreply.github.com>
2023-09-25 20:20:37 -0300

    Agregado de enlaces y mensajes emergentes

--- Portafolio4/codigo.js
+++ Portafolio4/codigo.js
@@ -1,2 +1,10 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+function mostrarNumero(){
+    alert(`0351 156754046`);
+}
+
+function mostrarCorreo(){
+    alert(`fraq86@gmail.com`);
+}

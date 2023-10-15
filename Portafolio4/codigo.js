3b0ed147bfa466515c52c78774cbee85a8c849cb
F. Rafael Alvarez <110036242+Rafael30586@users.noreply.github.com>
2023-10-15 20:17:28 -0300

    Eventos en el nombre

--- Portafolio4/codigo.js
+++ Portafolio4/codigo.js
@@ -1,5 +1,8 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+let nombre = document.getElementById("nombre");
+const textoEncabezado = document.createTextNode("Desarrollador Java");
+const textoEncabezadoOriginal = document.createTextNode("F. Rafael Alvarez");
 
 function mostrarNumero(){
     alert(`0351 156754046`);
@@ -8,3 +11,24 @@ function mostrarNumero(){
 function mostrarCorreo(){
     alert(`fraq86@gmail.com`);
 }
+
+function mostrarRol(){
+    //nombre.removeChild("textoEncabezadoOriginal");
+    nombre.removeChild(textoEncabezadoOriginal);
+	nombre.appendChild(textoEncabezado);
+}
+
+function mostrarNombre(){
+    nombre.removeChild(textoEncabezado);
+	nombre.appendChild(textoEncabezadoOriginal);
+}
+
+function mostrarNombre1(){
+    nombre.appendChild(textoEncabezadoOriginal);
+}
+
+mostrarNombre1();
+
+
+nombre.addEventListener("mouseover",mostrarRol)
+nombre.addEventListener("mouseout",mostrarNombre);

3a02db04f0c30da89aa8734bc122a6efb61a81b5
F. Rafael Alvarez <110036242+Rafael30586@users.noreply.github.com>
2023-10-16 10:40:11 -0300

    Tema claro incompleto

--- Portafolio4/codigo.js
+++ Portafolio4/codigo.js
@@ -3,9 +3,12 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 let nombre = document.getElementById("nombre");
 const cambioEstilo = document.getElementById("cambioEstilo");
 const body = document.getElementById("body");
+const navTab = document.getElementById("nav-tab");
+const contactos = document.getElementById("icon-grid");
 const textoEncabezado = document.createTextNode("Desarrollador Java");
 const textoEncabezadoOriginal = document.createTextNode("F. Rafael Alvarez");
 let estilo = 1;
+
 let navBar;
 
 function mostrarNumero(){
@@ -35,12 +38,17 @@ function mostrarNombre1(){
 }
 
 function cambiarEstilo(){
-    estilo += 1;
+    switch(estilo){
+        case 1: estilo+=1;break;
+        case 2: estilo-=1;break;
+    }
+    //estilo += 1;
 }
 
 function guardarElemento(){
     switch(estilo){
-        case 2: navBar = document.getElementById("encabezado");
+        case 1: navBar = document.getElementById("encabezado2");break;
+        case 2: navBar = document.getElementById("encabezado");break;
     }
     
 }
@@ -50,9 +58,22 @@ function cambiarColores(){
     guardarElemento();
 
     switch(estilo){
-        case 2: navBar.removeAttribute("id");
-        navBar.setAttribute("id","encabezado2")
+        case 1: body.style.backgroundColor = "#313131";
+        cambioEstilo.removeAttribute("class");
+        cambioEstilo.setAttribute("class","btn btn-outline-light me-2");
+        contactos.removeAttribute("class");
+        contactos.setAttribute("class","container px-4 py-5 rounded-top contenedor contactos");
+        break;
+        case 2: //navBar.removeAttribute("id");
+        //navBar.setAttribute("id","encabezado2")
         body.style.backgroundColor = "#f9f7f7";
+        cambioEstilo.removeAttribute("class");
+        cambioEstilo.setAttribute("class","btn btn-outline-secondary me-2");
+        contactos.removeAttribute("class");
+        contactos.setAttribute("class","container px-4 py-5 rounded-top contenedor contactos2");
+        //navTab.removeAttribute("class");
+        //navTab.setAttribute("class","nav nav-tabs rounded-top trayectoriaItems2 --bs-danger-text-emphasis");
+        break;
     }
 
 }

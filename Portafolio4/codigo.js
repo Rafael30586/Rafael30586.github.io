36e334bb02934f030648ca5e267aa0807cd336cd
F. Rafael Alvarez <110036242+Rafael30586@users.noreply.github.com>
2023-10-16 00:10:43 -0300

    Boton de estilo mas evento incompleto

--- Portafolio4/codigo.js
+++ Portafolio4/codigo.js
@@ -1,8 +1,12 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 let nombre = document.getElementById("nombre");
+const cambioEstilo = document.getElementById("cambioEstilo");
+const body = document.getElementById("body");
 const textoEncabezado = document.createTextNode("Desarrollador Java");
 const textoEncabezadoOriginal = document.createTextNode("F. Rafael Alvarez");
+let estilo = 1;
+let navBar;
 
 function mostrarNumero(){
     alert(`0351 156754046`);
@@ -30,8 +34,32 @@ function mostrarNombre1(){
     nombre.appendChild(textoEncabezadoOriginal);
 }
 
+function cambiarEstilo(){
+    estilo += 1;
+}
+
+function guardarElemento(){
+    switch(estilo){
+        case 2: navBar = document.getElementById("encabezado");
+    }
+    
+}
+
+function cambiarColores(){
+    cambiarEstilo();
+    guardarElemento();
+
+    switch(estilo){
+        case 2: navBar.removeAttribute("id");
+        navBar.setAttribute("id","encabezado2")
+        body.style.backgroundColor = "#f9f7f7";
+    }
+
+}
+
 mostrarNombre1();
 
 
 nombre.addEventListener("mouseover",mostrarRol)
 nombre.addEventListener("mouseout",mostrarNombre);
+cambioEstilo.addEventListener("click",cambiarColores);

42cc336dd0e49ab056c39ee5ef30ccd7eb630df5
F. Rafael Alvarez <110036242+Rafael30586@users.noreply.github.com>
2023-11-27 10:15:56 -0300

    Modificacion de directorio Raiz

---
+++ codigo.js
@@ -0,0 +1,8 @@
+boton = document.getElementById("boton");
+
+function irAlPortafolio(){
+    window.open("https://rafael30586.github.io/Portafolio4/");
+}
+
+
+boton.addEventListener("click",irAlPortafolio);
